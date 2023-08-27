099826eaea07180536d56dfc0af6ba49b9b44ea9
lleviackermann <supritkumar30@gmail.com>
2023-08-28 01:31:36 +0530

    register and login

---
+++ server/controllers/auth.js
@@ -0,0 +1,52 @@
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+import User from "../models/User.js"
+
+// Creating a account
+
+export const register = async (req, res) => {
+    try {
+        const {
+            firstName,
+            lastName,
+            email,
+            password,
+            picturePath,
+            friends, 
+            location,
+            occpation
+        } = req.body;
+
+        const salt = await bcrypt.genSalt();
+        const passwordHash = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            firstName,
+            lastName,
+            email,
+            password: passwordHash,
+            picturePath,
+            friends, 
+            location,
+            occpation,
+            viewedProfile: Math.floor(Math.random() * 10000),
+            impressions: Math.floor(Math.random() * 10000)
+        })
+
+        const savedUser = await newUser.save();
+        res.status(201).json(savedUser);
+    } catch(error) {
+        res.status(500).json({ error: error.message })
+    }
+};
+
+// Logging in
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email: email})
+        if(!user) res.status(400).json({ msg: "User does not exist. " });
+    } catch(err) {
+        res.status(500).json({ error: error.message })
+    }
+}
--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
-import exp from "constants";
+import { register } from "./controllers/auth.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -32,3 +32,16 @@ const storage = multer.diskStorage({
     }
 });
 const upload = multer({ storage });
+
+app.post("/auth/register", upload.single("picture"), register);
+
+app.use("/auth", authRoutes);
+
+const PORT = process.env.PORT || 6001
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    app.listen(PORT, () => { console.log("Server Started Successfully!")});
+}).catch((error) => console.log(error));
