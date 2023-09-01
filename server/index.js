4189fac1971932ee4405763109ca90ceffeb2376
lleviackermann <supritkumar30@gmail.com>
2023-09-02 01:57:47 +0530

    debugged

--- server/index.js
+++ server/index.js
@@ -10,8 +10,8 @@ import path from "path";
 import { fileURLToPath } from "url";
 import { register } from "./controllers/auth.js";
 import authRoutes from "./routes/auth.js";
-import userRoutes from "./routes/users.js"
-import postRoutes from "./routes/posts.js"
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
 import { createPost } from "./controllers/posts.js";
 import User from "./models/User.js";
 import Post from "./models/Post.js";
@@ -24,22 +24,22 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin"}));
-app.use(morgan("common"))
-app.use(bodyParser.json({ limit: "30mb", extended: true }))
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}))
-app.use(cors())
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, "public/assets");
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.originalname);
-    },
-  });
-  const upload = multer({ storage });
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
 
 app.post("/auth/register", upload.single("picture"), register);
 app.post("/posts", verifyToken, upload.single("picture"), createPost);
@@ -48,14 +48,19 @@ app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
 
-const PORT = process.env.PORT || 6001
+const PORT = process.env.PORT || 6001;
 
-mongoose.connect(process.env.MONGO_URL, {
+mongoose
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(() => {
-    app.listen(PORT, () => { console.log("Server Started Successfully!")});
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("Server Started Successfully!");
+    });
 
     // User.insertMany(users);
     // Post.insertMany(posts);
-}).catch((error) => console.log(error));
+  })
+  .catch((error) => console.log(error));
